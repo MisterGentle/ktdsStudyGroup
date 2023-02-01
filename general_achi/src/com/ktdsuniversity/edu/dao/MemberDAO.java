e2f4f5a7acdad6d1b873afd9d8b55b17c532423b
MisterGentle <tyhj1234@naver.com>
2023-02-02 00:01:38 +0900

    과제

---
+++ general_achi/src/com/ktdsuniversity/edu/dao/MemberDAO.java
@@ -0,0 +1,15 @@
+package com.ktdsuniversity.edu.dao;
+
+import java.util.List;
+
+import com.ktdsuniversity.edu.vo.MemberVO;
+
+public interface MemberDAO {
+	public int create(MemberVO memberVO);
+	
+	public List<MemberVO> readAll();
+	
+	public MemberVO read(int index);
+	
+	public MemberVO read(String valueOf);
+}
---
+++ general_achi/src/com/ktdsuniversity/edu/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.ktdsuniversity.edu.service;
+
+import java.util.List;
+
+import com.ktdsuniversity.edu.vo.MemberVO;
+
+public interface MemberService {
+
+	public boolean create(MemberVO memberVO);
+	
+	public List<MemberVO> readAll();
+	
+	public MemberVO read(int index);
+	
+	public MemberVO read(String valueOf);
+}
